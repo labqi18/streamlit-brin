2602d8b47ece2f768439aadfd33e60a84207f0a6
labqi18 <bgiqbalwardana18@gmail.com>
2025-10-24 13:43:02 +0700

    brin.py

--- brin.py
+++ brin.py
@@ -271,6 +271,7 @@ elif selected == "Visualization":
 
             # === Variabel yang digunakan di model SVR ===
             svr_vars = [
+                "Total Malaria Cases",
                 "Total  Population", 
                 "Forest and Water Areas (1000 Hectare)", 
                 "Average Temperature", 
